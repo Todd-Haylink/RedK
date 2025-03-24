591cd6b1194c00d619939e8f2cb65aa0907167d4
Todd-Haylink <57033631+Todd-Haylink@users.noreply.github.com>
2025-03-25 01:10:29 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
         /**
          * optional
          **/
-        short_name: "仙人掌主题",
+        short_name: "小辉网络科技",
         description: siteConfig.description,
         lang: siteConfig.lang,
         icon: "public/icon.svg", // the source for generating favicon & icons
